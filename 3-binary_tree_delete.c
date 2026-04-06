7f47a0bee57a835cacba759c088a4636681a6391
swatha <shamswathas@gmail.com>
2026-04-06 22:52:37 +1000

    delete tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/** binary_tree_delete - frees tree
+ * @tree: pointer to top of tree
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (!tree)
+		return;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
+}
