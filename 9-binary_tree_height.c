1f2d20da55e813df76888eb1ac9675a3d5dab56d
swatha <shamswathas@gmail.com>
2026-04-11 19:27:59 +1000

    correct outcome

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,11 +8,12 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left, right;
+	size_t result, left, right;
 
-	if (!tree)
+	if (!tree || (!tree->left && !tree->right))
 		return (0);
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
-	return (left > right ? left + 1 : right + 1);
+	result = left > right ? left : right;
+	return (result + 1);
 }
