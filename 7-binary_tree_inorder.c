35d01d014c6a2d78cc80c17eb9d89c6e5a785c00
swatha <shamswathas@gmail.com>
2026-04-11 18:19:37 +1000

    create normal order traversal method

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - traverse the tree from up to down, left to right
+ * @tree: pointer to node
+ * @func: function to apply to each node
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+	binary_tree_preorder(tree->left, func);
+	func(tree->n);
+	binary_tree_preorder(tree->right, func);
+}
